3c57a7dda432f59b3ed94c4d504c7d2749f8ff6f
Mikkel Luja Rasmussen <milr@itu.dk>
2019-11-07 16:00:37 +0100

    Made setup more user-friendly and added repository to package.json

--- setup.bat
+++ setup.bat
@@ -1,13 +1,28 @@
-REM Generates or overwrites the current config paths
+@echo off
+
+echo 1. Generates or overwrites the current config paths
 setlocal ENABLEDELAYEDEXPANSION
 set word=/
 set js=%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json
 set assets=%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState
 set js=%js:\=!word!%
 set assets=%assets:\=!word!%
-echo.{"jsonPath":"%js%","assetPath":"%assets%"} >"%cd%\config.json"
-REM Sets wte as an enviroment variable
+echo {"jsonPath":"%js%","assetPath":"%assets%"} > "%cd%\config.json"
+
+echo.
+echo SUCCESS: Path was configured.
+echo.
+
+echo 2. Set wte as an environment variable
 setx wte "node "%cd%\index.js""
-REM install libraries 
-npm install
+echo.
+
+echo 3. Installing libraries 
+echo.
+cmd /C "npm install"
+
+echo SUCCESS: Libraries was installed.
+echo.
+pause
+
 
