83a9d5822a0aad83b8d9d1ecb6a91801f1bb136b
Mikkel Luja Rasmussen <milr@itu.dk>
2020-05-07 12:02:54 +0200

    Updated to support v0.11.1121.0 - Update by pulling and running setup.bat again - Remember to make backups beforehand

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@
 echo 1. Generates or overwrites the current config paths
 setlocal ENABLEDELAYEDEXPANSION
 set word=/
-set js=%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json
+set js=%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 set assets=%appdata%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState
 set js=%js:\=!word!%
 set assets=%assets:\=!word!%
